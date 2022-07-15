cd1f9c24aceb8cd6a6997071ddd22d946c0a5b93
Ayan Antik Khan <ayanantikkhan@gmail.com>
2022-07-15 15:31:30 +0600

    Front-end added 😆

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Sidebar from './Sidebar'
+import User from './User';
+function App() {
+  return (
+    <div className="App">
+      {/* <Sidebar/> */}
+      <User/>
+    </div>
+  );
+}
+
+export default App;
