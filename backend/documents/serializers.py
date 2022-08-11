de80aeea0bbdda75bd63c2c165a25bd141d602cf
tanjim17 <tanjim.ak49@gmail.com>
2022-08-11 15:55:31 +0600

    wrote feed, recommend etc api

--- backend/documents/serializers.py
+++ backend/documents/serializers.py
@@ -1,8 +1,12 @@
 from .models import *
 from rest_framework import serializers
 
-
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
+        fields = '__all__'
+
+class FolderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Folder
         fields = '__all__'
--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -4,4 +4,10 @@ from .views import *
 urlpatterns = [
     path('create/', CreateDocumentView.as_view()),
     path('list/', ListDocumentView.as_view()),
+    path('move/', MoveDocumentView.as_view()),
+    path('addtag/', AddTagView.as_view()),
+    path('trending/', TrendingView.as_view()),
+    path('recommend/', RecommendView.as_view()),
+    path('createdir/', CreateFolderView.as_view()),
+    path('listdir/', ListFolderView.as_view()),
 ]
