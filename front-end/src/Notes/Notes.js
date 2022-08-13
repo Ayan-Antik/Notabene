67a4b555d16b306b917591d968eb919cbc4b1323
Fardin Hossain <fardinoyon123@gmail.com>
2022-08-14 01:20:16 +0600

    Adding Documents to Folder

--- front-end/src/Notes/Notes.js
+++ front-end/src/Notes/Notes.js
@@ -8,14 +8,94 @@ import './Notes.css'
 import Navbar from '../components/Navbar'
 import Iframe from 'react-iframe'
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import { Button, ListItemIcon } from '@mui/material';
 import { useParams } from 'react-router-dom'
 import axios from "axios"
 import Source from './search.png'
+import External from './external.png'
+import Folder from './folder.png'
 import { padding } from '@mui/system'
 import MDEditor from '@uiw/react-md-editor';
+import { IconButton } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import PersonIcon from '@mui/icons-material/Person';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors'
+import PropTypes from 'prop-types';
+import FolderIcon from '@mui/icons-material/Folder';
+
+
+
+/// FOR POPUP
+
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+
+function SimpleDialog(props, folderArray) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  console.log(folderArray);
+
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Select A Folder</DialogTitle>
+
+      <List sx={{ pt: 0 }}>
+
+        {/* {folderArray.map((folderName) => (
+          <ListItem key={folderName}>
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={folderName} />
+          </ListItem>
+        ))} */}
+
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Add account" />
+        </ListItem>
+
+      </List>
+
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
+
+/// FOR POPUP
+
 
 const Notes = () => {
+
+
   const {id} = useParams()
   
   const { user, logoutUser } = useContext(AuthContext);
@@ -51,11 +131,12 @@ const Notes = () => {
   useEffect(() => {
 	axios.get(`http://127.0.0.1:8000/highlight/list/?document__owner__username=&document__url=&document__id=${id}`).then((response) => {
 
-	//console.log(response.data);
+	console.log(response.data);
 	setHighlights(response.data);
   setData(response.data);
   //console.log(response.data[0].note);
   //setValue(response.data.note);
+  //console.log({data});
 
 	});
   }, []);
@@ -88,6 +169,60 @@ const Notes = () => {
   };
 
 
+
+
+  //// FOR POPUP
+
+  const [folders, setFolders] = useState([{}]);
+
+  // const [open, setOpen] = React.useState(false);
+  // const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
+
+  // const handleClose = (value) => {
+  //   setOpen(false);
+  //   setSelectedValue(value);
+  // };
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/documents/listdir/?owner__username=${user.username}`).then((response) => {
+  
+    console.log(response.data);
+    setFolders(response.data);
+    //setData(response.data);
+    //console.log(response.data[0].note);
+    //setValue(response.data.note);
+
+    //console.log(user.username);
+
+    //console.log(folders);
+  
+    });
+    }, []);
+
+
+  // //// FOR POPUP
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  //console.log(folders);
+
+
+
+
+
   return (
   	<div >
 
@@ -107,13 +242,17 @@ const Notes = () => {
 			<div style={{display: 'grid', gridTemplateColumns: 'max-content auto'}}>
 			
 				<span>
-					<img src={Source} width='32px'></img> 
+          <a href={data.url} target="_blank" rel="noopener noreferrer">
+          <img src= {External}
+          width='26px' 
+          onClick={data.url}></img></a>
+					 
 
 				</span>
 				<span style={{font: '24px bold', marginLeft: '16px'}}>
 			
 
-				<a href={data.url}>{data.url}</a>
+				{/* <a href={data.url}>{data.url}</a> */}
 				
 
 				</span>
@@ -122,6 +261,68 @@ const Notes = () => {
 				#
 			</h2>
 		</div>
+
+    {/* Code for popup */}
+
+    <div>
+      {/* <Typography variant="subtitle1" component="div">
+        Selected: {selectedValue}
+      </Typography>
+      <br /> */}
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add In A Folder
+      </Button>
+
+      <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Select A Folder</DialogTitle>
+
+      <List sx={{ pt: 0 }}>
+
+      {folders.map(function(folderName, i){
+
+
+		    return (<div key={i}>
+          
+          <ListItem key={i} 
+          button
+          onClick={
+            () => {
+              
+              axios
+              .patch(`http://localhost:8000/documents/${id}/update/`, {
+                  folder: folderName.id,
+              }, {
+                  headers: { 'Content-type': 'application/json' }
+              });
+
+
+            }}
+          >
+            {/* <img Source={FolderIcon}></img> */}
+            {/* <ListItemIcon children={FolderIcon}></ListItemIcon> */}
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                <FolderIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={folderName.name} />
+          </ListItem>
+
+          </div>
+      
+
+		    )
+
+
+	    })}
+
+
+      </List>
+
+    </Dialog>
+    </div>
+
+    {/* code for popup ends */}
 
         {myframe}
         {/* <img src={`${hostname}`}></img> */}
